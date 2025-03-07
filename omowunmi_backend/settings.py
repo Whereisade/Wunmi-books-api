5f06664aa4e16e719d6e6a0e20248bc87cc1d812
Whereisade <eniolaadeleke2303@gmail.com>
2025-03-07 09:15:29 +0100

    FEAT: added supabase2

--- omowunmi_backend/settings.py
+++ omowunmi_backend/settings.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-import environ
-import dj_database_url
+
+
 from decouple import config
-import socket
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
